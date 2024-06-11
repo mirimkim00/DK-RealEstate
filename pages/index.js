d424cab07862174f351420502007bb47a6ca435d
Mirim Kim <mirimkim00@gmail.com>
2024-06-10 20:32:48 -0400

    change carousel to swiper

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 import styles from '../styles/Home.module.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { Button, Carousel, Row, Col } from 'react-bootstrap';
 import Image from 'next/image';
@@ -12,17 +17,24 @@ export default function Home() {
   return (
     <>
       <section id={styles.hero}>
-        <Carousel
-          className={styles.heroCarousel}
-          prevIcon={false}
-          nextIcon={false}
-        >
-          <Carousel.Item className={styles.heroItem}>
+        <Swiper
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Autoplay, Pagination, Navigation]}
+          className={styles.heroSwiper}>
+          <SwiperSlide className={styles.heroItem}>
             <Image
               alt=''
               src={img1}
             />
-            <Carousel.Caption className={styles.heroCaption}>
+            <div className={styles.heroLabel}>
               <h1>First slide label</h1>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -31,14 +43,14 @@ export default function Home() {
                 LEARN MORE
                 <MdKeyboardArrowRight size={15} />
               </Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className={styles.heroItem}>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={styles.heroItem}>
             <Image
               alt=''
               src={img2}
             />
-            <Carousel.Caption className={styles.heroCaption}>
+            <div className={styles.heroLabel}>
               <h1>Second slide label</h1>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -47,14 +59,14 @@ export default function Home() {
                 LEARN MORE
                 <MdKeyboardArrowRight size={15} />
               </Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className={styles.heroItem}>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={styles.heroItem}>
             <Image
               alt=''
               src={img3}
             />
-            <Carousel.Caption className={styles.heroCaption}>
+            <div className={styles.heroLabel}>
               <h1>Third slide label</h1>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -63,14 +75,14 @@ export default function Home() {
                 LEARN MORE
                 <MdKeyboardArrowRight size={15} />
               </Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className={styles.heroItem}>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={styles.heroItem}>
             <Image
               alt=''
               src={img4}
             />
-            <Carousel.Caption className={styles.heroCaption}>
+            <div className={styles.heroLabel}>
               <h1>Fourth slide label</h1>
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -79,9 +91,9 @@ export default function Home() {
                 LEARN MORE
                 <MdKeyboardArrowRight size={15} />
               </Button>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+            </div>
+          </SwiperSlide>
+        </Swiper>
       </section>
 
       <section id={styles.info}>
@@ -95,84 +107,75 @@ export default function Home() {
 
       <section >
         <div id={styles.partners}>
-          <Carousel
-            className={styles.partnersCarousel}
-            prevIcon={false}
-            nextIcon={false}
-          >
-            <Carousel.Item className={styles.partnersItem}>
-              <div>
-                <Row className="gx-5" style={{ height: '300px' }}>
-                  <Col>
-                    <Image
-                      src={img1}
-                      alt="First slide"
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </Col>
-                  <Col className={styles.partnersCaption}>
-                    <h3>Partner 1</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Col>
-                </Row>
-              </div>
-            </Carousel.Item>
-            <Carousel.Item className={styles.partnersItem}>
-              <div>
-                <Row className="gx-5" style={{ height: '300px' }}>
-                  <Col>
-                    <Image
-                      src={img2}
-                      alt="First slide"
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </Col>
-                  <Col className={styles.partnersCaption}>
-                    <h3>Partner 2</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Col>
-                </Row>
-              </div>
-            </Carousel.Item>
-            <Carousel.Item className={styles.partnersItem}>
-              <div>
-                <Row className="gx-5" style={{ height: '300px' }}>
-                  <Col>
-                    <Image
-                      src={img3}
-                      alt="First slide"
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </Col>
-                  <Col className={styles.partnersCaption}>
-                    <h3>Partner 3</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Col>
-                </Row>
-              </div>
-            </Carousel.Item>
-            <Carousel.Item className={styles.partnersItem}>
-              <div>
-                <Row className="gx-5" style={{ height: '300px' }}>
-                  <Col>
-                    <Image
-                      src={img4}
-                      alt="First slide"
-                      layout="fill"
-                      objectFit="cover"
-                    />
-                  </Col>
-                  <Col className={styles.partnersCaption}>
-                    <h3>Partner 4</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Col>
-                </Row>
-              </div>
-            </Carousel.Item>
-          </Carousel>
+          <Swiper
+            loop={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            navigation={true}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Autoplay, Pagination, Navigation]}
+            className={styles.partnersSwiper}>
+            <SwiperSlide className={styles.partnersItem}>
+              <Row className="gx-5" style={{ height: '300px' }}>
+                <Col>
+                  <Image
+                    src={img1}
+                    alt="First slide"
+                  />
+                </Col>
+                <Col className={styles.partnersLabel}>
+                  <h3>Partner 1</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Col>
+              </Row>
+            </SwiperSlide>
+            <SwiperSlide className={styles.partnersItem}>
+              <Row className="gx-5" style={{ height: '300px' }}>
+                <Col>
+                  <Image
+                    src={img2}
+                    alt="Second slide"
+                  />
+                </Col>
+                <Col className={styles.partnersLabel}>
+                  <h3>Partner 2</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Col>
+              </Row>
+            </SwiperSlide>
+            <SwiperSlide className={styles.partnersItem}>
+              <Row className="gx-5" style={{ height: '300px' }}>
+                <Col>
+                  <Image
+                    src={img3}
+                    alt="Third slide"
+                  />
+                </Col>
+                <Col className={styles.partnersLabel}>
+                  <h3>Partner 3</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Col>
+              </Row>
+            </SwiperSlide>
+            <SwiperSlide className={styles.partnersItem}>
+              <Row className="gx-5" style={{ height: '300px' }}>
+                <Col>
+                  <Image
+                    src={img4}
+                    alt="Fourth slide"
+                  />
+                </Col>
+                <Col className={styles.partnersLabel}>
+                  <h3>Partner 4</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Col>
+              </Row>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </section>
     </>
