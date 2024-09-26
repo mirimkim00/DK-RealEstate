29a2b15f67ff6cfb0ea479c59b85a9f5232928e9
Mirim Kim <mirimkim00@gmail.com>
2024-09-26 19:37:36 -0400

    owners plans

--- pages/faq-owners.js
+++ pages/faq-owners.js
@@ -1,11 +1,9 @@
-import { FiMail } from "react-icons/fi";
-import { IoIosCall } from "react-icons/io";
-import { FaLongArrowAltRight, FaMapMarkerAlt } from "react-icons/fa";
 import styles from '../styles/Owners.module.css';
 import Accordion from 'react-bootstrap/Accordion';
 import menuPagePhoto from '../src/images/Menu_Page_Photo.jpg'
 import Image from 'next/image';
 import { Col, Row } from 'react-bootstrap';
+import SideBar from '@/components/SideBar';
 
 export default function FaqOwners() {
     const accordionData = [
@@ -32,49 +30,7 @@ export default function FaqOwners() {
 
             <section className={styles.faqSection}>
                 <Row className={styles.faqMain}>
-                    <Col lg='3' xl={3}>
-                        <div className={styles.sidebar}>
-                            <button>
-                                <FaLongArrowAltRight />
-                                &nbsp; Free Estimate
-                            </button>
-                            <button>
-                                <FaLongArrowAltRight />
-                                &nbsp; Non-Resident Taxes
-                            </button>
-                            <button>
-                                <FaLongArrowAltRight />
-                                &nbsp; Plans
-                            </button>
-                            <button>
-                                <FaLongArrowAltRight />
-                                &nbsp; Owners Portal
-                            </button>
-                        </div>
-
-                        <div className={styles.contactInfo}>
-                            <div>
-                                <IoIosCall />
-                                <p>
-                                    Local: 123-456-7890 <br />
-                                    Toll Free: 1-123-456-7890
-                                </p>
-                            </div>
-                            <div>
-                                <FiMail />
-                                <p>
-                                    DK@dk.ca
-                                </p>
-                            </div>
-                            <div>
-                                <FaMapMarkerAlt />
-                                <p>
-                                    123 Yonge St, Toronto
-                                    Ontario, M1M 1M1
-                                </p>
-                            </div>
-                        </div>
-                    </Col>
+                    <SideBar />
                     <Col>
                         <div className={styles.faq}>
                             <h1>Frequently Asked Questions</h1>
---
+++ pages/owners-plans.js
@@ -0,0 +1,53 @@
+import styles from '../styles/OwnersPlans.module.css';
+import menuPagePhoto from '../src/images/Menu_Page_Photo.jpg'
+import Image from 'next/image';
+import { Col, Row, Card, Button } from 'react-bootstrap';
+import SideBar from '@/components/SideBar';
+import { BsCheck } from "react-icons/bs";
+
+export default function OwnersPlans() {
+    const cardData = [1, 2, 3];
+    return (
+        <main id={styles.plans}>
+            <div className={styles.hero}>
+                <Image
+                    src={menuPagePhoto}
+                    alt=''
+                />
+                <div>
+                    <h1>Plans</h1>
+                </div>
+            </div>
+
+            <section className={styles.plansSection}>
+                <Row className={styles.plansMain}>
+                    <SideBar />
+                    <Col>
+                        <div className={styles.plans}>
+                            <h1>Plans</h1>
+                        </div>
+                        <Row className="justify-content-between">
+                            {cardData.map((index) => (
+                                <Card key={index} style={{ width: '18rem' }}>
+                                    <Card.Body className={styles.planCard}>
+                                        <Card.Title>Plan {index}</Card.Title>
+                                        <Card.Text>CA$ XX</Card.Text>
+                                        <Card.Text>+ HST</Card.Text>
+                                        <Card.Text>
+                                            <BsCheck />Feature<br />
+                                            <BsCheck />Feature<br />
+                                            <BsCheck />Feature<br />
+                                            <BsCheck />Feature<br />
+                                            <BsCheck />Feature<br />
+                                            <BsCheck />Feature<br />
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            ))}
+                        </Row>
+                    </Col>
+                </Row>
+            </section>
+        </main >
+    )
+}
