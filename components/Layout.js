26fa22f59e2035f7855853581dc5096ab34b7fbe
bkim <126510650+bkim89229@users.noreply.github.com>
2024-10-28 21:33:32 -0400

    Add files via upload

--- components/Layout.js
+++ components/Layout.js
@@ -1,42 +1,13 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import MainNav from "./MainNav";
-import HeroImgSection from "./HeroImgSection";
-import Sidebar from "./Sidebar";
-import SubContent from "./SubContent";
 import Footer from "./Footer";
+import MainNav from "./MainNav";
 
 export default function Layout(props) {
-  const router = useRouter();
-  const isMainPage = router.pathname === "/";
-  const [currentPage, setCurrentPage] = useState("Owners");
-
-  useEffect(() => {
-    const path = router.pathname.replace("/", "") || "Owners";
-    setCurrentPage(path.charAt(0).toUpperCase() + path.slice(1)); // 첫 글자 대문자로
-  }, [router.pathname]);
-
   return (
     <>
       <title>DK Real Estate Home</title>
-      <MainNav setCurrentPage={setCurrentPage} />
-
-      {isMainPage ? (
-        <div>{props.children}</div>
-      ) : (
-        <div className="layoutContainer">
-          <HeroImgSection />
-          <div className="contentContainer">
-            <div className="sidebar">
-              <Sidebar currentPage={currentPage} />
-            </div>
-            <div className="subContent">
-              <SubContent currentPage={currentPage} />
-            </div>
-          </div>
-        </div>
-      )}
-
+      <MainNav />
+      {props.children}
+      <br />
       <Footer />
     </>
   );
--- components/MainNav.js
+++ components/MainNav.js
@@ -1,17 +1,9 @@
-import { useRouter } from "next/router";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
 
-export default function MainNav({ setCurrentPage }) {
-  const router = useRouter();
-
-  const handleNavClick = (page, path) => {
-    setCurrentPage(page);
-    router.push(path);
-  };
-
+function MainNav() {
   return (
     <Navbar expand="lg">
       <Container fluid>
@@ -25,33 +17,17 @@ export default function MainNav({ setCurrentPage }) {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll" style={{ justifyContent: "end" }}>
           <Nav className="mr-auto my-2 my-lg-0">
-            <Nav.Link onClick={() => handleNavClick("Owners", "/owners")}>
-              Owners
-            </Nav.Link>
-            <Nav.Link onClick={() => handleNavClick("Tenants", "/tenants")}>
-              Tenants
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => handleNavClick("Properties", "/properties")}
-            >
-              Properties
-            </Nav.Link>
-            <Nav.Link
-              onClick={() =>
-                handleNavClick("New Development", "/new_development")
-              }
-            >
-              New Development
-            </Nav.Link>
-            <Nav.Link onClick={() => handleNavClick("Plans", "/plans")}>
-              Plans
-            </Nav.Link>
-            <Nav.Link onClick={() => handleNavClick("Contact", "/contact")}>
-              Contact
-            </Nav.Link>
+            <Nav.Link href="/faq-owners">Owners</Nav.Link>
+            <Nav.Link href="#">Tenants</Nav.Link>
+            <Nav.Link href="#">Properties</Nav.Link>
+            <Nav.Link href="#">New Development</Nav.Link>
+            <Nav.Link href="#">Plans</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
+
+export default MainNav;
