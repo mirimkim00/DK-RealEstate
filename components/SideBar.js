e21311a9640cc3f2ac05bfe8632f7f56473970c2
bkim <126510650+bkim89229@users.noreply.github.com>
2024-11-10 03:25:26 -0500

    Add files via upload

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,84 +1,66 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
 import { FiMail } from "react-icons/fi";
 import { IoIosCall } from "react-icons/io";
 import { FaLongArrowAltRight, FaMapMarkerAlt } from "react-icons/fa";
 import styles from "../styles/Owners.module.css";
 
-export default function Sidebar({ currentPage }) {
+export default function Sidebar({}) {
+  const router = useRouter();
+
+  const getSidebarMenu = () => {
+    switch (router.pathname) {
+      case "/owners/faq-owners":
+        return [
+          { text: "FAQ", path: "/faq-owners/faq" },
+          { text: "Free Estimate", path: "/faq-owners/free-estimate" },
+          { text: "Plans", path: "/faq-owners/plans" },
+          { text: "Owners Portal", path: "/faq-owners/owners-portal" },
+        ];
+      case "/tenants":
+        return [
+          { text: "1", path: "/tenants/1" },
+          { text: "2", path: "/tenants/2" },
+          { text: "3", path: "/tenants/3" },
+        ];
+      case "/properties":
+        return [
+          { text: "1", path: "/tenants/1" },
+          { text: "2", path: "/tenants/2" },
+          { text: "3", path: "/tenants/3" },
+        ];
+      case "/new-development":
+        return [
+          { text: "1", path: "/tenants/1" },
+          { text: "2", path: "/tenants/2" },
+          { text: "3", path: "/tenants/3" },
+        ];
+      case "/contact":
+        return [
+          { text: "1", path: "/tenants/1" },
+          { text: "2", path: "/tenants/2" },
+          { text: "3", path: "/tenants/3" },
+        ];
+      default:
+        return [
+          { text: "1", path: "/tenants/1" },
+          { text: "2", path: "/tenants/2" },
+          { text: "3", path: "/tenants/3" },
+        ];
+    }
+  };
+
   return (
-    <div>
+    <>
       <div className={styles.sidebar}>
-        {currentPage === "Owners" && (
-          <>
+        {getSidebarMenu().map((button, index) => (
+          <Link key={index} href={button.path}>
             <button>
               <FaLongArrowAltRight />
-              Owners1
+              &nbsp; {button.text}
             </button>
-            <button>
-              <FaLongArrowAltRight />
-              Owners2
-            </button>
-          </>
-        )}
-        {currentPage === "Tenants" && (
-          <>
-            <button>
-              <FaLongArrowAltRight />
-              Tenants1
-            </button>
-            <button>
-              <FaLongArrowAltRight />
-              Tenants2
-            </button>
-          </>
-        )}
-        {currentPage === "Properties" && (
-          <>
-            <button>
-              <FaLongArrowAltRight />
-              Properties1
-            </button>
-            <button>
-              <FaLongArrowAltRight />
-              Properties2
-            </button>
-          </>
-        )}
-        {currentPage === "New Development" && (
-          <>
-            <button>
-              <FaLongArrowAltRight />
-              New Development1
-            </button>
-            <button>
-              <FaLongArrowAltRight />
-              New Development2
-            </button>
-          </>
-        )}
-        {currentPage === "Plans" && (
-          <>
-            <button>
-              <FaLongArrowAltRight />
-              Plans1
-            </button>
-            <button>
-              <FaLongArrowAltRight />
-              Plans2
-            </button>
-          </>
-        )}
-        {currentPage === "Contact" && (
-          <>
-            <button>
-              <FaLongArrowAltRight />
-              Contact1
-            </button>
-            <button>
-              <FaLongArrowAltRight />
-              Contact2
-            </button>
-          </>
-        )}
+          </Link>
+        ))}
       </div>
 
       <div className={styles.contactInfo}>
@@ -98,6 +80,6 @@ export default function Sidebar({ currentPage }) {
           <p>123 Yonge St, Toronto Ontario, M1M 1M1</p>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- components/SubpageHero.js
+++ components/SubpageHero.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import menuPagePhoto from "../src/images/1.jpg";
+import menuPagePhoto from "../src/images/5.jpg";
 import styles from "../styles/Owners.module.css";
 
 export default function SubepageHero() {
